87693c18d2182fc6475b01aac694ce113a7f6a63
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-05-07 16:09:44 +1000

    commit tree 840164dfbd84b73b22269c20b009085c0855c740

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result;
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+  } else if (400 < someValue && someValue <= 2000 ) {
+    result = 'That will be twenty bucks.';
+  } else if (someValue > 2000 && someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else  if (2500 < someValue){
+    result = 'No can do.';
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result = (city == 'NYC') ? "Ok, sounds good." : "No go.";
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(message){
+  let result;
+
+  switch(message) {
+    case 'generous': 
+      result = "Thank you so much.";
+      break;
+    case 'not as generous':
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye."
+  }
+  return result
 }
